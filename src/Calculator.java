63d511eb25cf005f840315e1e6c92301fd1171d6
Andr3i04 <amoldovan710@gmail.com>
2025-04-01 09:27:16 +0300

    lab6

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,24 +9,42 @@ public class Calculator {
         rezultat = x;
     }
 
-    void adunare(double x) {
+    public Calculator adunare(double x) {
         rezultat += x;
+        return this;
     }
 
-    void scadere(double x) {
+    public Calculator scadere(double x) {
         rezultat -= x;
+        return this;
     }
 
-    void inmultire(double x) {
+    public Calculator inmultire(double x) {
         rezultat *= x;
+        return this;
     }
 
-    void impartire(double x) {
+    public Calculator impartire(double x) {
         if (x == 0) {
             throw new RuntimeException("Impartire la 0!!!!!");
         } else {
             rezultat /= x;
+            return this;
         }
+    }
+
+    public Calculator radical() {
+        if (rezultat < 0) {
+            throw new RuntimeException("Nu se poate");
+        } else {
+            rezultat = Math.sqrt(rezultat);
+            return this;
+        }
+    }
+
+    public Calculator exponent() {
+        rezultat = Math.exp(rezultat);
+        return this;
     }
 
     public double getRezultat() {
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         // Calculator c = new Calculator();
-        AdvancedCalculator ac = new AdvancedCalculator();
 
-        ac.adunare(7);
-        ac.inmultire(2);
-        ac.radical();
-        ac.exponent();
+    /*
+        c.adunare(7);
+        c.inmultire(2);
+        c.radical();
+        c.exponent();
 
         System.out.println(ac.getRezultat());
-
+    */
+        System.out.println(new Calculator(3).scadere(2).adunare(7).inmultire(2).getRezultat());
     }
 }
